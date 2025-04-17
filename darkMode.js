1096177057b1c69ff96cd1fb959380b73ca8a115
NathanaelLoop <Jinderloop@gmail.com>
2025-04-17 11:31:42 -0400

    90%

---
+++ darkMode.js
@@ -0,0 +1,6 @@
+let buttonDM = document.querySelector('#buttonDM');
+
+buttonDM.addEventListener("click", switchDarkMode);
+
+function switchDarkMode(){
+    document.documentElement.classList.toggle("dark")}
